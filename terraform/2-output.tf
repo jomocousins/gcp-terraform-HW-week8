871a8c10708cd0ef0dfcc80a5abdfe8e711f882f
jomocousins <jomo1.cousins@gmail.com>
2026-05-07 22:56:10 -0400

    Update 2-output.tf

--- terraform/2-output.tf
+++ terraform/2-output.tf
@@ -1,1 +1,27 @@
+output "vm_internal_ip" {
+  description = "Internal IP address of the VM"
+  value       = google_compute_instance.week8_vm.network_interface[0].network_ip
+}
 
+output "vm_external_ip" {
+  description = "External IP address of the VM"
+  value       = google_compute_instance.week8_vm.network_interface[0].access_config[0].nat_ip
+}
+
+output "vm_name" {
+  description = "Name of the VM"
+  value       = google_compute_instance.week8_vm.name
+
+}
+
+output "vm_id" {
+  description = "Identifier for the resource"
+  value       = google_compute_instance.week8_vm.id
+
+}
+
+output "self_link" {
+  description = " URI of the created resource"
+  value       = google_compute_instance.week8_vm.self_link
+
+}
