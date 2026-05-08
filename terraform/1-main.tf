2d434930e7e628af832cef586e5f2f9febd0045e
jomocousins <jomo1.cousins@gmail.com>
2026-05-07 22:55:00 -0400

    Update 1-main.tf

--- terraform/1-main.tf
+++ terraform/1-main.tf
@@ -1,1 +1,25 @@
+resource "google_compute_instance" "wk8_vm" {
+  name         = "week8-vm"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-stream-10"
+      size  = "100"
+    }
+  }
+
+
+  network_interface {
+    network = "default"
+
+    access_config {}
+  }
+
+  tags = ["http-server"] 
+
+  metadata_startup_script = file("./startup.sh") 
+}
+
 
